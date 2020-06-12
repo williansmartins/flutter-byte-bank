547e2ea379943e099a63593a3942f9c7b31f7b15
Willians Martins <professorwillians@gmail.com>
2020-06-12 02:21:37 -0300

    rollback

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -3,13 +3,13 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 Future<Database> getDatabase() async {
-  final String path = join(await getDatabasesPath(), 'bytebank2.db');
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
   return openDatabase(
     path,
     onCreate: (db, version) {
       db.execute(ContactDao.tableSql);
     },
-    version: 2,
+    version: 1,
   );
 }
 
