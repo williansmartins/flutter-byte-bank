0494776023767f3dd9147a8768698239f154b1eb
Willians Martins <professorwillians@gmail.com>
2020-06-12 00:45:40 -0300

    add assets e botao

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,31 @@ void main() {
 }
 
 class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Dashboard'),
-        ), 
+        ),
         body: Column(
           children: <Widget>[
-            Image.network(
-                'https://cdn.pixabay.com/photo/2019/10/15/06/03/pinwheel-4550711_960_720.jpg')
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.people),
+                  Text('Contacts')
+                ],
+              ),
+            )
           ],
-        ), 
-      ), 
-    ); 
+        ),
+      ),
+    );
   }
 }
